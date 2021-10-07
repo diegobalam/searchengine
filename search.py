69b44b6923e3b8ce28659cf8b17a48b1663ff9b9
diegobalam <85011366+diegobalam@users.noreply.github.com>
2021-10-07 02:23:33 -0500

    Update search.py

--- search.py
+++ search.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 import json
 import pandas as pd
 
-save_path='/home/diegobalam/Fake_news/data/'
+save_path='/home/balam/balam/Fake_news/data/'
 options = webdriver.FirefoxOptions()
 options.add_argument('-headless')
  
